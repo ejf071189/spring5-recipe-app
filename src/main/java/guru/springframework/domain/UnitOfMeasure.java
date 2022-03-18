55b1705a0cc4fe5882869bf96390a9487d134cc5
Evan Fredericks <evan.fredericks@gmail.com>
2022-03-18 16:00:04 -0500

    one to one ingredient unitOfMeasure

--- src/main/java/guru/springframework/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -2,57 +2,33 @@ package guru.springframework.domain;
 
 import java.math.BigDecimal;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 
 @Entity
-public class Ingredient {
-    
+public class UnitOfMeasure {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String description;
     private BigDecimal amount;
-
-    @ManyToOne
-    private Recipe recipe;
-
-    @OneToOne
-    private Ingredient ingredient;
-
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
         this.description = description;
     }
-
     public BigDecimal getAmount() {
         return amount;
     }
-
     public void setAmount(BigDecimal amount) {
         this.amount = amount;
     }
 
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
+    
 }
